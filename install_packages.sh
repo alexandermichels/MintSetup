8ea8a4fd7f965c2e1e87cf6765ea5fd2069388b6
Alexander Michels <alexandercm4297@gmail.com>
2018-03-17 22:59:45 -0400

    MikTex setup

--- install_packages.sh
+++ install_packages.sh
@@ -13,11 +13,14 @@ sudo apt-get -y install curl
 #install MiKTeX
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys D6BC243565B2087BC3F897C9277A7293F59E4889
 echo "deb http://miktex.org/download/ubuntu xenial universe" | sudo tee /etc/apt/sources.list.d/miktex.list
-sudo apt-get -y update -y
-sudo apt-get -y install miktex -y
+sudo apt-get -y update
+sudo apt-get -y install miktex
+miktexsetup finish
+initexmf --set-config-value [MPM]AutoInstall=1
+mpm --verbose --package-level=complete --upgrade
 
 #install texstudio
-sudo apt-get -y texstudio
+sudo apt-get -y install texstudio
 
 #install R
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys E084DAB9
