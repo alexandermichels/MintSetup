d7b2aba3e2092314940335c5f13c462a1fe3bcff
Alexander Michels <alexandercm4297@gmail.com>
2018-03-18 00:15:23 -0400

    Merged a helper script into main script

--- r_packages.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-sudo R -e 'install.packages("mosaic", repos="http://cran.us.r-project.org")'
-sudo R -e 'install.packages("rmarkdown", repos="http://cran.us.r-project.org")'
-sudo R -e 'install.packages("Lock5Data", repos="http://cran.us.r-project.org")'
-sudo R -e 'install.packages("ggformula", repos="http://cran.us.r-project.org")'
